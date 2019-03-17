7340573e454f2c6f9a7c4bcc322713056460f24d
gokhanyigit <bmgokhanyigit@gmail.com>
2019-03-18 00:17:49 +0300

    Version 2.0.0.0

---
+++ YigitFramework.Northwind.Business/Abstract/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YigitFramework.Northwind.Entities.Concrete;
+
+namespace YigitFramework.Northwind.Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        Product GetById(int id);
+        Product Add(Product product);
+    }
+}
---
+++ YigitFramework.Northwind.Business/Concrete/Managers/ProductManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YigitFramework.Northwind.Business.Abstract;
+using YigitFramework.Northwind.DataAccess.Abstract;
+using YigitFramework.Northwind.Entities.Concrete;
+
+namespace YigitFramework.Northwind.Business.Concrete.Managers
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public Product Add(Product product)
+        {
+            return _productDal.Add(product);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.ProductId == id);
+        }
+    }
+}
